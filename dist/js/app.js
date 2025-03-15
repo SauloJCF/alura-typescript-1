ea6f7ba6d7d82e331214fac65ba14dc27cc2c642
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-03-15 16:58:51 -0300

    Criação controller e vinculação evento

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,7 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
-import { Negociacao } from './models/negociacao.js';
-
 const negociacaoController = new NegociacaoController();
-
 const formulario = document.querySelector('.form');
-
 formulario.addEventListener('submit', (event) => {
-  event.preventDefault();
-  negociacaoController.adicionar();
+    event.preventDefault();
+    negociacaoController.adicionar();
 });
---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,12 @@
+import { Negociacao } from '../models/negociacao.js';
+export class NegociacaoController {
+    constructor() {
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = document.querySelector('#quantidade');
+        this._inputValor = document.querySelector('#valor');
+    }
+    adicionar() {
+        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        console.log(negociacao);
+    }
+}
