b3de0a324b222eb05231061fed1ec753eb9f9e1c
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-04-18 19:23:59 -0300

    Classe e método abstrato

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
@@ -11,7 +11,5 @@ export class View<T> {
     this.elemento.innerHTML = template;
   }
 
-  template(model: T): string {
-    throw new Error("Método 'Template' deve ser implementado em classe filha!");
-  }
+  abstract template(model: T): string;
 }
