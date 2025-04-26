c807dcc57ffbcf8ca28f49f5a3567739d95da4ea
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-04-26 20:27:23 -0300

    Decorators parte 3

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -4,6 +4,22 @@ export function logarTempoDeExecucao() {
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
+    const metodoOriginal = descriptor.value;
+
+    descriptor.value = function () {
+      const t1 = performance.now();
+
+      const retorno = metodoOriginal();
+
+      const t2 = performance.now();
+
+      console.log(
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`
+      );
+
+      return retorno;
+    };
+
     return descriptor;
   };
 }
