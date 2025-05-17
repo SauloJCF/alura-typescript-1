8f9b4c9880df6ac7e885bd382adc64de649bf305
Saulo Jose Coelho de Figueiredo <saulojcf@gmail.com>
2025-05-17 19:47:02 -0300

    Adiciona interface Comparavel e implementa método ehIgual nas classes Negociacao e Negociacoes para comparação de objetos.

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
+import { Comparavel } from '../interfaces/comparavel.js';
 import { Imprimivel } from '../utils/imprimivel.js';
 import { Negociacao } from './negociacao.js';
 
-export class Negociacoes implements Imprimivel {
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes> {
   private negociacoes: Negociacao[] = [];
 
   public adiciona(negociacao: Negociacao) {
@@ -14,5 +15,11 @@ export class Negociacoes implements Imprimivel {
 
   public paraTexto(): string {
     return JSON.stringify(this, null, 2);
+  }
+
+  ehIgual(objeto: Negociacoes): Boolean {
+    return (
+      JSON.stringify(this.negociacoes) === JSON.stringify(objeto.negociacoes)
+    );
   }
 }
